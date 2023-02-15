41d7d650358f6e6d422e81c1a391463732773c4a
ual1prekrasneyshyy <uali03.alm@gmail.com>
2023-02-15 17:16:05 +0600

    Lab 3

--- Lab 3/ToDo App/script.js
+++ Lab 3/ToDo App/script.js
@@ -1,13 +1,29 @@
-let toDoList = ['Do '];
+let toDoList = ['Do laboratory work for Web Development', 'Install the WebStorm', 'Go to the library'];
 
 function fillTheList(){
     let printedList = '';
 
-    for(let task in toDoList){
-        printedList += '<div><input type="checkbox"><p>task</p><button>DELETE</button></div>'
+    for(let i = 0; i < toDoList.length; i++){
+        printedList +=
+            '<div><input type="checkbox"><p>'
+            + toDoList[i] +
+            '</p><button onclick="deleteTask('+i+')">DELETE</button></div>'
     }
 
     document.getElementById('list').innerHTML = printedList;
+}
+
+
+function addNewTask(){
+    toDoList.push(document.getElementById('new_task').value);
+    document.getElementById('new_task').value = '';
+    fillTheList();
+}
+
+
+function deleteTask(i){
+    toDoList.splice(i, 1);
+    fillTheList();
 }
 
 fillTheList();
