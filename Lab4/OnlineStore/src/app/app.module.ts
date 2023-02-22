d025b4a71c8a910def8a2eaa3b3f614cbc49045e
ual1prekrasneyshyy <uali03.alm@gmail.com>
2023-02-22 21:49:18 +0600

    Lab 4: Online Store

--- Lab4/OnlineStore/src/app/app.module.ts
+++ Lab4/OnlineStore/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { IgxCarouselModule } from 'igniteui-angular';
 import { AppComponent } from './app.component';
 import {AppRoutingModule} from "./app-routing.module";
 import { ProductsComponent } from './components/products/products.component';
@@ -12,7 +12,8 @@ import { ProductsComponent } from './components/products/products.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    IgxCarouselModule
   ],
   providers: [],
   bootstrap: [AppComponent]
