afe93d18d0537998eca7d2fdd088826955bb3e4b
Uali <uali03.alm@gmail.com>
2023-03-01 12:21:50 +0600

    Lab 5

--- Lab 5/OnlineStore/src/app/app.module.ts
+++ Lab 5/OnlineStore/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import {AppRoutingModule} from "./app-routing.module";
+import {ProductsComponent} from "./components/products/products.component";
+import {ProductListComponent} from "./components/product-list/product-list.component";
+import {ProductItemComponent} from "./components/product-item/product-item.component";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductsComponent,
+    ProductListComponent,
+    ProductItemComponent
   ],
   imports: [
     BrowserModule,
---
+++ Lab 5/OnlineStore/src/app/components/product-list/product-list.component.ts
@@ -0,0 +1,12 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'product-list',
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.css']
+})
+export class ProductListComponent implements OnInit {
+  ngOnInit(): void {
+  }
+
+}
