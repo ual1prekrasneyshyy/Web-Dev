ea37a5f509e7283dd971bb676218dc2ad366fe6d
l10n <uali03.alm@gmail.com>
2023-03-08 02:03:18 +0600

    Lab 6

--- Lab 6/Album/src/app/components/albums/albums.component.ts
+++ Lab 6/Album/src/app/components/albums/albums.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {AlbumsService} from "../../services/albums.service";
+import {Album} from "../../models/album";
 
 @Component({
   selector: 'app-albums',
   templateUrl: './albums.component.html',
   styleUrls: ['./albums.component.css']
 })
-export class AlbumsComponent {
+export class AlbumsComponent{
+
+  albums: Album[] = [];
+  error = false
+
+  constructor(private albums_service: AlbumsService) {
+    this.albums_service.get_all_albums().subscribe(albums => {
+      this.albums = albums;
+    }, error => {
+      this.error = true
+    });
+  }
 
 }
--- Lab 6/Album/src/app/services/albums.service.ts
+++ Lab 6/Album/src/app/services/albums.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
+import {Album} from "../models/album";
 
 @Injectable({
   providedIn: 'root'
 })
 export class AlbumsService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  public get_all_albums(): Observable<Album[]> {
+    return this.http.get<Album[]>('https://jsonplaceholder.typicode.com/albums')
+  }
+
+  public get_album_by_id(album_id: number): Observable<Album> {
+    return this.http.get<Album>(`https://jsonplaceholder.typicode.com/albums/${album_id}`)
+  }
 }
