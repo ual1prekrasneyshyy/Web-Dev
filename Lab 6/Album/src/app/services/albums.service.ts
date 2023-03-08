82ecc956945388055a22ce2cdbf5772964f5eed7
l10n <uali03.alm@gmail.com>
2023-03-08 18:03:20 +0600

    Lab 6

--- Lab 6/Album/src/app/services/albums.service.ts
+++ Lab 6/Album/src/app/services/albums.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {Observable} from "rxjs";
 import {Album} from "../models/album";
+import {Photo} from "../models/photo";
 
 @Injectable({
   providedIn: 'root'
@@ -11,10 +12,14 @@ export class AlbumsService {
   constructor(private http: HttpClient) { }
 
   public get_all_albums(): Observable<Album[]> {
-    return this.http.get<Album[]>('https://jsonplaceholder.typicode.com/albums')
+    return this.http.get<Album[]>('https://jsonplaceholder.typicode.com/albums');
   }
 
   public get_album_by_id(album_id: number): Observable<Album> {
-    return this.http.get<Album>(`https://jsonplaceholder.typicode.com/albums/${album_id}`)
+    return this.http.get<Album>(`https://jsonplaceholder.typicode.com/albums/${album_id}`);
+  }
+
+  public get_photos_by_album_id(album_id: number): Observable<Photo[]> {
+    return this.http.get<Photo[]>(`https://jsonplaceholder.typicode.com/albums/${album_id}/photos`);
   }
 }
