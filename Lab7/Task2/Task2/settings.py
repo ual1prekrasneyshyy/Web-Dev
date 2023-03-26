288fa39b75e6d7a731e0b043eb7560e3c91989fb
ual1prekrasneyshyy <uali03.alm@gmail.com>
2023-03-26 22:56:07 +0600

    Lab 7: Task 2

--- Lab7/Task2/Task2/settings.py
+++ Lab7/Task2/Task2/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-o3b+v!8b+0bzwn_hu4u!^_eehlnihjekjhw-aoaol8#-h5*h+c
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'app'
 ]
 
 MIDDLEWARE = [
@@ -104,7 +105,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru-kz'
 
 TIME_ZONE = 'UTC'
 
@@ -117,6 +118,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
--- Lab7/Task2/app/admin.py
+++ Lab7/Task2/app/admin.py
@@ -2,3 +2,7 @@ from django.contrib import admin
 from .models import Post
 
 admin.site.register(Post)
+
+
+#login: ual1prekrasneyshyy
+#password: uali
