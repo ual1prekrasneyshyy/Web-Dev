ae5208274c25a8ab38dc4797dbab4df2dc4ab62a
ual1prekrasneyshyy <uali03.alm@gmail.com>
2023-03-26 21:51:34 +0600

    Lab 7: Task 2

--- Lab7/Task2/Task2/wsgi.py
+++ Lab7/Task2/Task2/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for MyFirstDjangoApp project.
+WSGI config for Task2 project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'MyFirstDjangoApp.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Task2.settings')
 
 application = get_wsgi_application()
