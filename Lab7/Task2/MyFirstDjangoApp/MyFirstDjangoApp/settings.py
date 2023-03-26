646ac6cde02fae89994ab55a4b3bb215a00c04e4
ual1prekrasneyshyy <uali03.alm@gmail.com>
2023-03-26 21:28:54 +0600

    Lab 7: Task 2

--- Lab7/Task2/MyFirstDjangoApp/MyFirstDjangoApp/settings.py
+++ Lab7/Task2/MyFirstDjangoApp/MyFirstDjangoApp/settings.py
@@ -104,9 +104,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru-ru'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Almaty'
 
 USE_I18N = True
 
@@ -117,6 +117,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
