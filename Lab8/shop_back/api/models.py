6d19b1feb74f440dc419d674441b39e3ca8049bc
ual1prekrasneyshyy <uali03.alm@gmail.com>
2023-04-05 15:42:57 +0600

    Lab8

--- Lab8/shop_back/api/models.py
+++ Lab8/shop_back/api/models.py
@@ -4,6 +4,10 @@ from django.db import models
 class Category(models.Model):
     name = models.CharField(max_length=255, default='')
 
+    class Meta:
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
+
     def to_json(self):
         return {
             'id': self.id,
