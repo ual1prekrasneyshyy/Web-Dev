b37d287e8e4948931e3d12f2673732034906b4fa
ual1prekrasneyshyy <uali03.alm@gmail.com>
2023-04-19 01:30:46 +0600

    Lab 10

--- Lab10/front/hh-back/src/app/app.module.ts
+++ Lab10/front/hh-back/src/app/app.module.ts
@@ -1,14 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { CompaniesComponent } from './components/companies/companies.component';
+import { CompanyComponent } from './components/company/company.component';
+import { TopVacanciesComponent } from './components/top-vacancies/top-vacancies.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CompaniesComponent,
+    CompanyComponent,
+    TopVacanciesComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
