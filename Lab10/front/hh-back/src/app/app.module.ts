a0893dc7a76df40bbafae8a11649e29a2b5f3d43
ual1prekrasneyshyy <uali03.alm@gmail.com>
2023-04-19 01:37:51 +0600

    Lab 10

--- Lab10/front/hh-back/src/app/app.module.ts
+++ Lab10/front/hh-back/src/app/app.module.ts
@@ -6,6 +6,9 @@ import { AppComponent } from './app.component';
 import { CompaniesComponent } from './components/companies/companies.component';
 import { CompanyComponent } from './components/company/company.component';
 import { TopVacanciesComponent } from './components/top-vacancies/top-vacancies.component';
+import {HttpClientModule} from "@angular/common/http";
+import {CompaniesService} from "./services/companies.service";
+import {VacanciesService} from "./services/vacancies.service";
 
 @NgModule({
   declarations: [
@@ -16,9 +19,13 @@ import { TopVacanciesComponent } from './components/top-vacancies/top-vacancies.
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [
+    CompaniesService,
+    VacanciesService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- Lab10/front/hh-back/src/app/services/vacancies.service.ts
+++ Lab10/front/hh-back/src/app/services/vacancies.service.ts
@@ -1,9 +1,13 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
 
 @Injectable({
   providedIn: 'root'
 })
 export class VacanciesService {
 
-  constructor() { }
+  private BASE_URL: string;
+  constructor(private http: HttpClient) {
+    this.BASE_URL = 'http://localhost:8000/api/vanacies/';
+  }
 }
