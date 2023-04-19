26068cdbea0d150fb65fd711effb3bbb1c51f633
ual1prekrasneyshyy <uali03.alm@gmail.com>
2023-04-19 21:55:25 +0600

    Lab 10

--- Lab10/front/hh-back/src/app/components/company/company.component.ts
+++ Lab10/front/hh-back/src/app/components/company/company.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute} from "@angular/router";
+import {CompaniesService} from "../../services/companies.service";
+import {Company} from "../../models/company";
 
 @Component({
   selector: 'app-company',
   templateUrl: './company.component.html',
   styleUrls: ['./company.component.css']
 })
-export class CompanyComponent {
+export class CompanyComponent implements OnInit{
+
+  company: Company;
+  loaded: boolean;
+  not_founded: boolean;
+
+  constructor(private router: ActivatedRoute, private companiesService: CompaniesService) {
+    this.company = {} as Company;
+    this.not_founded = false;
+    this.loaded = false;
+  }
+  ngOnInit(): void {
+    this.router.paramMap.subscribe((params) => {
+      this.companiesService.get_company_by_id(parseInt(<string>params.get('id'))).subscribe((company) => {
+        this.company = company;
+        this.loaded = true;
+      }, error => {
+        this.loaded = true;
+        this.not_founded = true;
+      });
+    });
+  }
+
+
 
 }
